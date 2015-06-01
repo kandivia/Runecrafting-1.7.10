f03496a14949540c7f4834c096edc8db8200dd35
hansbe123 <hanspistor@gmail.com>
2015-05-31 23:27:15 -0400

    This didn't commit for some reason -_-

---
+++ src/main/java/com/kandivia/runecrafting/player/PlayerEventHandler.java
@@ -0,0 +1,15 @@
+package com.kandivia.runecrafting.player;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraftforge.event.entity.EntityEvent.EntityConstructing;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+
+public class PlayerEventHandler {
+	
+	@SubscribeEvent
+	public void onEntityConstructing(EntityConstructing event) {
+		if (event.entity instanceof EntityPlayer && ExtendedPlayer.get((EntityPlayer) event.entity) == null) {
+			ExtendedPlayer.register((EntityPlayer)event.entity);
+		}
+	}
+}
