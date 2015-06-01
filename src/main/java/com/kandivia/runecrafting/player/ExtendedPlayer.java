ac5cf1302c1b29c5f00be54b6a1ccb4d2d6dcbec
hansbe123 <hanspistor@gmail.com>
2015-05-31 23:25:11 -0400

    Keeping Track of EXP
    works. Right click talisman to see current Exp in chat, and for now
    right click spellbook for free exp

--- src/main/java/com/kandivia/runecrafting/player/ExtendedPlayer.java
+++ src/main/java/com/kandivia/runecrafting/player/ExtendedPlayer.java
@@ -5,67 +5,68 @@ import java.lang.ref.WeakReference;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.ChatComponentText;
 import net.minecraft.world.World;
 import net.minecraftforge.common.IExtendedEntityProperties;
 
 import com.kandivia.runecrafting.main.Reference;
 
 public class ExtendedPlayer implements IExtendedEntityProperties {
-	
+
 	public static final String PROPERTIESNAME = Reference.MOD_ID;
-	
+
 	public WeakReference<EntityPlayer> player;
-	
+
 	private int currentExp, currentLevel, expToLevel;
-	
+
 	public ExtendedPlayer() {
-		
+
 	}
-	
+
 	public ExtendedPlayer(EntityPlayer player) {
 		this.player = new WeakReference<EntityPlayer>(player);
 		currentExp = currentLevel = 0;
-		expToLevel = 1;
+		expToLevel = 2;
 	}
-	
-	public final void register(EntityPlayer player) {
+
+	public final static void register(EntityPlayer player) {
 		player.registerExtendedProperties(PROPERTIESNAME, new ExtendedPlayer(player));
 	}
-	
-	public final ExtendedPlayer get(EntityPlayer player) {
+
+	public final static ExtendedPlayer get(EntityPlayer player) {
 		return (ExtendedPlayer) player.getExtendedProperties(PROPERTIESNAME);
 	}
-	
+
 
 	@Override
 	public void saveNBTData(NBTTagCompound compound) {
-		
+
 		NBTTagCompound properties = new NBTTagCompound();
 		properties.setInteger("currentExp", currentExp);
 		properties.setInteger("currentLevel", currentLevel);
 		properties.setInteger("expToLevel", expToLevel);
-		
+
 		compound.setTag(PROPERTIESNAME, properties);
 		System.out.println("[Runecrafting]\tSaving NBT Data");
-		
+
 	}
 
 	@Override
 	public void loadNBTData(NBTTagCompound compound) {
 		NBTTagCompound properties = (NBTTagCompound) compound.getTag(PROPERTIESNAME);
-		
+
 		this.currentExp = properties.getInteger("currentExp");
 		this.currentLevel = properties.getInteger("currentLevel");
 		this.expToLevel = properties.getInteger("expToLevel");
-		
+
 		System.out.println("[Runecrafting]\tLoading NBT Data");
 	}
 
 	@Override
 	public void init(Entity entity, World world) {
-		
+
 	}
-	
+
 	public boolean addExp(int amount) {
 		boolean level = this.currentExp + amount >= this.expToLevel;
 		this.currentExp += amount;
@@ -75,9 +76,19 @@ public class ExtendedPlayer implements IExtendedEntityProperties {
 			this.expToLevel *= 2;
 			return true;
 		}
+		player.get().addChatComponentMessage(new ChatComponentText("EXP to level " + (this.expToLevel-this.currentExp)));
 		return false;
+	}
+
+	public void printExp() {
+		System.out.println("Printing");
+		player.get().addChatComponentMessage(new ChatComponentText("Current EXP is " + this.currentExp));
 
 	}
 	
+	public int getLevel() {
+		return this.currentLevel;
+	}
+
 
 }
